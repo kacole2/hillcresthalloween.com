b38b9fe5c025b3a1ac830a55c3cbcc4e6fb14026
Kendrick Coleman <kendrickcoleman@gmail.com>
2014-09-25 19:41:29 -0400

    using jQuery to load headers and footers

--- js/main.js
+++ js/main.js
@@ -10,14 +10,19 @@
 
 //Mobile Navbar
 $(document).ready(function(){
-        $("#nav-mobile").html($("#nav-main").html());
-        $("#nav-trigger span").click(function(){
-          if ($("nav#nav-mobile ul").hasClass("expanded")) {
-              $("nav#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
-              $(this).removeClass("open");
-          } else {
-              $("nav#nav-mobile ul").addClass("expanded").slideDown(250);
-              $(this).addClass("open");
-          }
-        });
-      });
+  $('#header').load('header.html');
+
+  $("#nav-mobile").html($("#nav-main").html());
+
+  $("#nav-trigger span").click(function(){
+    if ($("nav#nav-mobile ul").hasClass("expanded")) {
+        $("nav#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
+        $(this).removeClass("open");
+    } else {
+        $("nav#nav-mobile ul").addClass("expanded").slideDown(250);
+        $(this).addClass("open");
+    }
+  });
+
+  $('#footer').load('footer.html');
+});
